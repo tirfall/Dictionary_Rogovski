3051a32691b3e75abb821165bcc72c05982cbbf8
opilane <opilane@HP-CZC2349HTB>
2023-02-27 15:02:19 +0200

    Add project files.

---
+++ Dictionary_Rogovski.py
@@ -0,0 +1,20 @@
+﻿from module1 import *
+
+riik_pealinn,pealinn_riik=loeson("riigid_pealinnd.txt")
+while True:
+    choice = int(input("0 - otse riik või pealinn\n1 - otse riik või pealinn\n"))
+    if choice == 0:
+        slovo = str(input("Sisesta riigi või pealinn: "))
+        slovo.title
+        if slovo.isalpha():
+            finddict(slovo,riik_pealinn,pealinn_riik)
+        else:
+            print("Value error")
+    elif choice == 1:
+        riik_pealinn,pealinn_riik=loeson("riigid_pealinnd.txt")
+        print(riik_pealinn)
+        print(pealinn_riik)
+    elif choice == 2:
+        pass
+
+        
---
+++ module1.py
@@ -0,0 +1,20 @@
+﻿def finddict(slovo,riik_pealinn,pealinn_riik):
+    loeson
+    x = riik_pealinn.get(slovo)
+    if x == None:
+        y =  pealinn_riik.get(slovo)
+        print(y)
+    else:
+        print(x)
+
+def loeson(f:str):
+    riik_pealinn={}
+    pealinn_riik={}
+    file = open (f, 'r', encoding="utf-8")
+    for line in file:
+        k, v=line.strip().split('-')
+        riik_pealinn[k] = v
+        pealinn_riik[v] = k
+        
+    file.close()
+    return riik_pealinn,pealinn_riik
